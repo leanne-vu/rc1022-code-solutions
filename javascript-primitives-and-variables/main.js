9242b37588b642cae606d88f8cd9b4297a676716
Leanne Vu <leannevu1@yahoo.com>
2022-10-17 16:45:26 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Leanne Vu';
+
+var isCool = true;
+
+var totalPets = 0;
+
+var unknown = null;
+
+var never;
+
+console.log('Full Name:', fullName);
+console.log('Cool?', isCool);
+console.log('Total pets:', totalPets);
+console.log('Unknown:', unknown);
+console.log('Undefined:', never);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+console.log('typeof unknown:', typeof unknown);
+console.log('typeof Undefine:', typeof never);
