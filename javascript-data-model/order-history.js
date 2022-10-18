4ea54fb0addfa5570b09c7e18a2e8da6349fb47e
Leanne Vu <leannevu1@yahoo.com>
2022-10-18 17:05:33 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,65 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34',
+    shipTo: 'JS Masher',
+    orderNum: '144-3941689-8772232',
+    delivered: 'August 8,2020',
+    deliveryMessage: null,
+    picture: null,
+    title: 'Javascript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnWindowClosed: 'Sep 7,2020',
+    indivTotal: '$31.55'
+  },
+  {
+    orderPlaced: 'July 19,2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNum: '113-9984268-128057',
+    delivered: 'Jul 20,2020',
+    deliveryMessage: 'Your package was delivered. It was handed directly to a resident.',
+    picture: null,
+    name: 'The Timeless Way of Building',
+    returnWindowClosed: 'Aug 19,2020',
+    indivTotal: '$41.33'
+  },
+  {
+    orderPlaced: 'July 4,2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNum: '114-2875557-9059409',
+    delivered: 'Jul 7,2020',
+    deliveryMessage: 'Your package was delivered. It was handed directly to a resident.',
+    picture: null,
+    name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    returnWindowClosed: 'Aug 5,2020',
+    indivTotal: '$15.98'
+  },
+  [{
+    orderPlaced: 'July 3,2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNum: '113-2883177-2648248',
+    delivered: 'Jul 5,2020',
+    deliveryMessage: null,
+    picture: null,
+    name: 'GameCube Controller- Super Smash Bros. Edition (Nintendo Switch)',
+    returnWindowClosed: 'Aug 4,2020',
+    indivTotal: '$94.95'
+  },
+  {
+    orderPlaced: 'July 3,2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNum: '113-2883177-2648248',
+    delivered: 'Jul 5,2020',
+    deliveryMessage: null,
+    picture: null,
+    title: 'The Art of Sql',
+    returnWindowClosed: 'Aug 4,2020',
+    indivTotal: '$33.99'
+  }]
+];
+
+console.log(orderHistory);
