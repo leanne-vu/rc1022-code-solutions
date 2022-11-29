a8b9c50c67ddfd4f12c623c23e521ebc50c38561
Leanne Vu <leannevu1@yahoo.com>
2022-11-29 23:59:16 +0000

    Completed node-require exercise.

---
+++ node-require/add.js
@@ -0,0 +1,6 @@
+function add(number1, number2) {
+  var result = number1 + number2;
+  return 'result: ' + result;
+}
+
+module.exports = add;
---
+++ node-require/divide.js
@@ -0,0 +1,7 @@
+function divide(number1, number2) {
+  var result = number1 / number2;
+  return 'result: ' + result;
+
+}
+
+module.exports = divide;
