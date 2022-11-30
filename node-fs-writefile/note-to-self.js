0f40c2b058eac91d6dfda9a4d1c57be074a5dd55
Leanne Vu <leannevu1@yahoo.com>
2022-11-30 22:30:23 +0000

    Completed node-fs-writefile exercise.

---
+++ node-fs-writefile/note-to-self.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const data = process.argv[2] + '\n';
+fs.writeFile('note.txt', data, err => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
---
+++ node-fs-writefile/write-rand.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const data = Math.random() + '\n';
+
+fs.writeFile('random.txt', data, err => {
+  if (err) { console.error(err); process.exit(1); }
+});
