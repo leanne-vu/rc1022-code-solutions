45a7b70f27d59e9e7b51e23ca68ecddf1a74ae13
Leanne Vu <leannevu1@yahoo.com>
2022-12-02 07:28:42 +0000

    Completed string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -6,28 +6,60 @@
 // if the word is smaller than 4, lowercase all
 // return and join method//
 
+// function titleCase(title) {
+//   var words = title.split(' ');
+
+//   for (var i = 0; i < words.length; i++) {
+//     words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
+
+//     if (words[i].length >= 3 && words[i] !== 'THE') { words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase(); }
+//     if (words[i].length < 3 && words[i] !== 'AN') { words[i] = words[i].toLowerCase(); }
+//     if (words[i] === 'Javascript' || words[i] === 'javascript') { words[i] = 'JavaScript'; }
+//     if (words[i] === 'Javascript:' || words[i] === 'javascript:') { words[i] = 'JavaScript:'; }
+//     if (words[i] === 'Api') { words[i] = words[i].toUpperCase(); }
+//     if (words[i] === 'And' || words[i] === 'or' || words[i] === 'nor' || words[i] === 'but' || words[i] === 'a' || words[i] === 'tHe' || words[i] === 'ThE' || words[i] === 'the' || words[i] === 'For') { words[i] = words[i].toLowerCase(); }
+//     if (words[i] === 'i') { words[i] = words[i].toUpperCase(); }
+//     if (words[i].includes('-')) {
+//       words[i] = words[i].toLowerCase().replace(/(?:^|[\s-/])\w/g, function (match) {
+//         return match.toUpperCase();
+//       });
+//     }
+//     if (words[i].includes(':')) {
+
+//       words[i + 1] = words[i + 1].charAt(0).toUpperCase() + words[i + 1].slice(1).toLowerCase();
+//       console.log(words[i + 1]);
+//     }
+
+//   }
+//   return words.join(' ');
+// }
 function titleCase(title) {
   var words = title.split(' ');
+  var small = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   for (var i = 0; i < words.length; i++) {
-    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
-    if (words[i].length >= 3) { words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase(); }
-    if (words[i].length < 3) { words[i] = words[i].toLowerCase(); }
-    if (words[i] === 'Javascript' || words[i] === 'javascript') { words[i] = 'JavaScript'; }
-    if (words[i] === 'Javascript:' || words[i] === 'javascript:') { words[i] = 'JavaScript:'; }
-    if (words[i] === 'Api') { words[i] = words[i].toUpperCase(); }
-    if (words[i] === 'And' || words[i] === 'or' || words[i] === 'nor' || words[i] === 'but' || words[i] === 'a' || words[i] === 'an' || words[i] === 'The' || words[i] === 'For') { words[i] = words[i].toLowerCase(); }
-    if (words[i] === 'i') { words[i] = words[i].toUpperCase(); }
-    if (words[i].includes('-')) {
-      words[i] = words[i].toLowerCase().replace(/(?:^|[\s-/])\w/g, function (match) {
-        return match.toUpperCase();
-      });
+    var replace = '';
+    var lowerword = words[i].toLowerCase();
+    var previousWord = words[i - 1];
+    if (small.includes(lowerword) && i !== 0 && previousWord[previousWord.length - 1] !== ':') {
+      replace = lowerword;
+    } else if (!small.includes(lowerword) || words[i] === words[0] || previousWord[previousWord.length - 1] === ':') {
+      replace += words[i][0].toUpperCase();
+      replace += words[i].substring(1).toLowerCase();
     }
-    if (words.indexOf(':')) {
-      var currentIndex = words.indexOf(':');
-      words[currentIndex + 1] = words[currentIndex + 1].charAt(0).toUpperCase() + words[currentIndex + 1].slice(1).toLowerCase();
-      if (words[currentIndex] === 'javascript:' || words[currentIndex] === 'Javascript:') {
-        words[currentIndex] = 'JavaScript:';
-      }
+    if (replace.toLowerCase() === 'javascript') {
+      replace = 'JavaScript';
     }
+    if (replace.toLowerCase() === 'javascript:') {
+      replace = 'JavaScript:';
+    }
+    if (replace.toLowerCase() === 'api') {
+      replace = 'API';
+    }
+    if (replace.includes('-')) {
+      replace = replace.split('-');
+      replace[1] = replace[1][0].toUpperCase() + replace[1].substring(1);
+      replace = replace[0] + '-' + replace[1];
+    }
+    words[i] = replace;
   } return words.join(' ');
 }
