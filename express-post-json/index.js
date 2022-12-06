30cd42c655ddbb0371b183e8100604a5b22a5fdc
Leanne Vu <leannevu1@yahoo.com>
2022-12-06 18:44:26 +0000

    Updated express-post-json so object is an object.

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 var nextId = 1;
-const grades = [];
+const grades = {};
 
 const app = express();
 app.get('/api/grades', (req, res) => {
-  res.status(200).json(grades);
+  var array = [];
+  for (var key in grades) {
+    array.push(grades[key]);
+  }
+  res.status(200).json(array);
 }
 );
 
 app.use(express.json());
 app.post('/api/grades', (req, res) => {
-  grades.push(req.body);
+  grades[nextId] = (req.body);
   req.body.id = nextId;
   nextId++;
   res.sendStatus(201);
