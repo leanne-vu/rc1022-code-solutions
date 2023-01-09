6a8b2a8e431f1122a018b5d00348b095906bbf56
Leanne Vu <leannevu1@yahoo.com>
2023-01-09 23:57:44 +0000

    Fixed some big o algorithm numbers.

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -8,10 +8,10 @@ function uniqueLinear(words) {
     i < words.length;               // 2 * n = O(2n) ~= 0(n)
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 1 * 1 = O(1)
-    if (!seen[word]) {              // 1 * n = O(1)
-      seen[word] = true;            // 3 * n = O(3n) ~= 0(n)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
+    const word = words[i];          // 2 * n = O(n) ~= 0(n)
+    if (!seen[word]) {              // 2 * n = O(n) ~= 0(n)
+      seen[word] = true;            // 1 * n = 0(n) ~= 0(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
@@ -24,20 +24,20 @@ function uniqueQuadratic(words) {
     i < words.length;               // 2 * n = O(2n) ~= 0(n)
     i++                             // 2 * n = O(2n) ~= 0(n)
   ) {
-    const word = words[i];          // 1 * 1 = O(1)
-    let isUnique = true;            // 1 * 1 = O(1)
+    const word = words[i];          // 2 * n = O(2n) ~= 0(n)
+    let isUnique = true;            // 1 * n = O(n)
     for (
       let c = 0;                    // 1 * n = O(n)
-      c < i;                        // 2 * n * n = O(2n^2) ~= 0(n^2)
-      c++                           // 2 * n * n = O(2n^2) ~=0(n^2)
+      c < i;                        // 1 * n * n = O(n^2)
+      c++                           // 2 * n * n = O(n^2) ~=0(n^2)
     ) {
-      const comparing = words[c];   // 1 * 1 = O(1)
-      if (comparing === word) {     // 3 * n = O(3n) ~= 0(n)
-        isUnique = false;           // 3 * 1 = O(3) ~= 0(1)
+      const comparing = words[c];   // 2 * n * n = O(n^2)
+      if (comparing === word) {     // 1 * n * n  = O(n^2)
+        isUnique = false;           // 1 * n * n  = O(n^2)
       }
     }
-    if (isUnique) {                 // 4 * n = O(4n) ~= 0(n)
-      unique[unique.length] = word; // 4 * 1 = O(4) ~= 0(1)
+    if (isUnique) {                 // 1* n = 0(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
